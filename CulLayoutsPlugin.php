25b694a736b1e4d78db02ea92faa86d5442ed57a
Frederic Duby <fcd1@columbia.edu>
2015-03-31 13:12:01 -0400

    Remove test layout

--- CulLayoutsPlugin.php
+++ CulLayoutsPlugin.php
@@ -8,8 +8,6 @@ class CulLayoutsPlugin extends Omeka_Plugin_AbstractPlugin
   public function filterExhibitLayouts($layouts) 
   {
     
-    $layouts['fred-layout-one'] = array( 'name' => 'Fred First Layout',
-					 'description' => 'This is the first layout by Fred' );
     $layouts['file-no-text'] = array( 'name' => 'File without Text',
 					 'description' => 'Layout featuring file(s), no text' );
     return $layouts;
