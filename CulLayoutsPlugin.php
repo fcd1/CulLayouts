2d05878151feeb1d5c1090ec26c698ea5cc11b8a
Frederic Duby <fcd1@columbia.edu>
2015-05-01 12:32:18 -0400

    Remove content block layout: file-no-text
    The file-no-text content block layout has been replaced by
    the following content block layout: cul-file-no-text. Same
    functionality, just a name change.

--- CulLayoutsPlugin.php
+++ CulLayoutsPlugin.php
@@ -8,8 +8,6 @@ class CulLayoutsPlugin extends Omeka_Plugin_AbstractPlugin
   public function filterExhibitLayouts($layouts) 
   {
     
-    $layouts['file-no-text'] = array( 'name' => 'File without Text - DO NOT USE',
-					 'description' => 'Layout featuring file(s), no text, one file per row' );
     $layouts['cul-file-no-text'] = array( 'name' => 'CUL - File without Text',
 					 'description' => 'CUL Layout featuring file(s), no text, one file per row' );
     return $layouts;
