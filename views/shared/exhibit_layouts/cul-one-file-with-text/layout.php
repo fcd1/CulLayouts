72475f001c09b0ea6038aea072b088d068294e08
Frederic Duby <fcd1@columbia.edu>
2015-05-06 18:14:53 -0400

    Use only the first attachment, plus minor CSS change.
    Since this content block layout should only display one item and
    its associated descriptive text, the code was changed to only look
    at the first attachement (i.e. item), even though the input form
    allows you to enter more than one item -- items are entered via a
    button which uses javascript (original Exhibit Builder code, which
    we would prefer to leave alone), and this javascript allows multiple
    items to be entered.
    
    CSS changes: add some padding at the top of the item block.

--- views/shared/exhibit_layouts/cul-one-file-with-text/layout.php
+++ views/shared/exhibit_layouts/cul-one-file-with-text/layout.php
@@ -13,11 +13,17 @@ $textBehavior = isset($options['text-behavior'])
     : 'column';
 ?>
 <div class="exhibit-items <?php echo $position; ?> <?php echo $width; ?> captions-<?php echo $captionPosition; ?> <?php echo $textBehavior; ?>">
-    <?php foreach ($attachments as $attachment): ?>
      <div class="exhibit-item">
-<?php echo $this->exhibitAttachment($attachment, array('imageSize' => 'fullsize') ) ?>
+<?php 
+
+if ( isset($attachments[0]) ) {
+
+  echo $this->exhibitAttachment($attachments[0], array('imageSize' => 'fullsize'));
+
+}
+
+?>
      </div>
-    <?php endforeach; ?>
 </div>
 <div class="exhibit-text <?php echo $position; ?> <?php echo $width; ?> <?php echo $textBehavior; ?> ">
   <?php echo $text; ?>
