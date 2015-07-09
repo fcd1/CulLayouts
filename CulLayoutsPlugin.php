f483d1a83bfb9f3f354ee1fad9183d66ad12a458
Frederic Duby <fcd1@columbia.edu>
2015-07-09 11:55:55 -0400

    Disable the cul-one-file-with-text and cul-gallery-no-text layouts
    It was decided that these new block layouts may be confusing to
    users. Code implementing the layouts was not removed or commented
    out. Instead, the entries for the layouts in questions were commented
    out of the exhibit_layouts filter.
    Note that the leftover code will not pose a problem since it is in
    subdirs specific to the layouts in question, and this code will
    not be exercised while the layouts are disabled.
    To enable the layouts, just remove the comments around the code
    populating the entry in the exhibit_layouts filter.

--- CulLayoutsPlugin.php
+++ CulLayoutsPlugin.php
@@ -10,10 +10,18 @@ class CulLayoutsPlugin extends Omeka_Plugin_AbstractPlugin
     
     $layouts['cul-file-no-text'] = array( 'name' => 'CUL - File without Text',
 					 'description' => 'CUL Layout featuring file(s), no text, one file per row' );
+    // fcd1, 09Jul15: It was decided that the cul-one-file-with-text and cul-gallery-no-text block layouts
+    // would be disabled at this time -- these new block layouts could be confusing to users.
+    // However, code implementing the two layouts has not been removed or commented out. Note that this code is within
+    // subdirs specific to the layouts in question, and should never be exercised while the layouts are disabled --
+    // commenting out the entries for the layuouts in this method disables the layouts.
+    // To enable one or both layouts, activate the appropriate lines of code below by removing them from the comment block.
+    /*
     $layouts['cul-one-file-with-text'] = array( 'name' => 'CUL - One File with Text',
 					 'description' => 'CUL Layout featuring one file with text' );
     $layouts['cul-gallery-no-text'] = array( 'name' => 'CUL - Gallery with no text',
 				     'description' => 'CUL gallery' );
+    */
     return $layouts;
 
   }
